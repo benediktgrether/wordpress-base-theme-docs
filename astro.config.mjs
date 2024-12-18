2c012d343d4ba4c615767d813659b84484a6f93f
Benedikt <info@benediktgrether.de>
2024-12-18 22:20:06 +0100

    Add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://benediktgrether.github.io/wordpress-base-theme-docs',
+    site: 'https://benediktgrether.github.io/wordpress-base-theme-docs/',
 
     integrations: [
         starlight({
